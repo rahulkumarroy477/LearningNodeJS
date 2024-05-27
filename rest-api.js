2a6648cc81bef6fa84b39c2de6149150422a3561
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-05-27 12:53:58 +0530

    updated post return id of updated movie

--- rest-api.js
+++ rest-api.js
@@ -5,7 +5,10 @@ let movies = require("./mock-data.json");
 const PORT = 8000;
 
 // Middleware
+
+// middleware1
 app.use(express.urlencoded({ extended: false }));
+// middleware2
 app.use(express.json()); // To parse JSON bodies
 
 // Ensure logs directory exists
@@ -64,17 +67,22 @@ app.get("/movies/:id", (req, res) => {
 app.post("/api/movies", (req, res) => {
     const now = new Date();
     const body = req.body;
-    movies.push({ id: movies.length + 1, ...body });
+    const newMovieId = movies.length+1
+    movies.push({ id: newMovieId, ...body });
     const content = `POST ${now.getTime()} --> ${req.path} visited, ${JSON.stringify(body)} sent\n`;
     fs.appendFile(`./logs/log-${now.getDate()}.txt`, content, (err) => {
         if (err) console.error("Failed to write log:", err);
     });
     fs.writeFile("./mock-data.json", JSON.stringify(movies, null, 2), (err) => {
         if (err) return res.json({ status: "Failed", error: err.message });
-        return res.json({ status: "Done" });
+        return res.json({ 
+            id:newMovieId,
+            status: "Done" });
     });
 });
 
+
+// PATCH
 app.patch("/api/movies/:id", (req, res) => {
     const now = new Date();
     const movieId = Number(req.params.id);
@@ -90,20 +98,23 @@ app.patch("/api/movies/:id", (req, res) => {
         });
         return res.status(404).json({ message: "Movie not found" })
     }
-    console.log(movieIndex);
+    let prevData = movies[movieIndex];
+    console.log(prevData);
     const updatedMovie = { ...movies[movieIndex], ...updates };
     console.log(updatedMovie);
     movies[movieIndex] = updatedMovie;
     fs.writeFile("./mock-data.json", JSON.stringify(movies, null, 2), (err) => {
         if (err) return res.json({ status: "Failed", error: err.message });
 
-        content = `PATCH ${now.getTime()} --> ${req.path} visited, ${JSON.stringify(updates)} successful\n`;
+        content = `PATCH ${now.getTime()} --> ${req.path} visited,${JSON.stringify(prevData)} changed to ${JSON.stringify(updatedMovie)} successful\n`;
         fs.appendFile(`./logs/log-${now.getDate()}.txt`, content, (err) => {
             if (err) console.error("Failed to write log:", err);
         });
         return res.json(updatedMovie);
     });
 });
+
+// DELETE
 
 app.delete("/api/movies/:id", (req, res) => {
     const now = new Date();
